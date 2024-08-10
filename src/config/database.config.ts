1dec9f80ab6f907224655922947bc6b1ace008c8
Josip Bjezancevic <josip.bjezancevic@gmail.com>
2024-08-10 17:54:50 +0200

    Fix database.config

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -3,7 +3,7 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('database', () => ({
   host: process.env.DATABASE_HOST || 'localhost',
   port: parseInt(process.env.DATABASE_PORT) || 5432,
-  user: process.env.POSTGRES_USER,
+  user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   name: process.env.DATABASE_NAME,
   synchronize: process.env.DATABASE_SYNC === 'true' ? true : false,
